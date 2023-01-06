933eb20ab843931c252f03520f71ca56e594f0f2
utsav <utsavvasoya89@gmail.com>
2023-01-06 18:24:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const socket = require('socket.io')
+const localStorage = require('localStorage')
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-var port = process.env.PORT || 3001;
+var port = 3001;
 
 
 
@@ -17,13 +18,20 @@ app.get('/', (req, res) => {
 app.post('/room', (req, res) => {
     roomname = req.body.roomname;
     username = req.body.username;
+    localStorage.setItem("username", username);
+    localStorage.setItem("roomname", roomname);
     res.redirect(`/room?username=${username}&roomname=${roomname}`)
 })
 
-app.get('/room', (req, res)=>{
-    res.render('room')
+app.get('/room', (req, res) => {
+    var username = localStorage.getItem("username");
+    var roomname = localStorage.getItem("roomname");
+    res.render('room', { username: username, roomname: roomname })
 })
 
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
     console.log(`Server Running on port ${port}`)
 })
+const io = socket(server);
+require('./utils/socket')(io);
--- public/chat.js
+++ public/chat.js
@@ -5,8 +5,7 @@ const feedback = document.getElementById('feedback');
 const roomMessage = document.querySelector('.room-message');
 const users = document.querySelector('.users');
 
-//Socket server URL
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://localhost:3001');
 
 //Fetch URL Params from URL
 const queryString = window.location.search;
@@ -15,9 +14,6 @@ const username = urlParams.get('username');
 const roomname = urlParams.get('roomname');
 console.log(username, roomname);
 
-//Display the roomname the user is connected to
-roomMessage.innerHTML = `Connected in room ${roomname}`
-
 //Emitting username and roomname of newly joined user to server
 socket.emit('joined-user', {
     username: username,
@@ -25,7 +21,7 @@ socket.emit('joined-user', {
 })
 
 //Sending data when user clicks send
-send.addEventListener('click', () =>{
+send.addEventListener('click', () => {
     socket.emit('chat', {
         username: username,
         message: message.value,
@@ -36,29 +32,42 @@ send.addEventListener('click', () =>{
 
 //Sending username if the user is typing
 message.addEventListener('keypress', () => {
-    socket.emit('typing', {username: username, roomname: roomname})
+    socket.emit('typing', { username: username, roomname: roomname })
 })
+message.addEventListener("keyup", () => {
+    socket.emit("stopTyping", { username: username, roomname: roomname });
+});
 
 //Displaying if new user has joined the room
-socket.on('joined-user', (data)=>{
-    output.innerHTML += '<p>--> <strong><em>' + data.username + ' </strong>has Joined the Room</em></p>';
+socket.on('joined-user', (data) => {
+    if (username == data.username) {
+        output.innerHTML += '<p class="center"><strong><em>' + "You" + ' </strong>Joined the Room</em></p><br>';
+    } else {
+        output.innerHTML += '<p class="center"><strong><em>' + data.username + ' </strong>has Joined the Room</em></p><br>';
+    }
 })
 
 //Displaying the message sent from user
 socket.on('chat', (data) => {
-    output.innerHTML += '<p><strong>' + data.username + '</strong>: ' + data.message + '</p>';
+    if (username == data.username) {
+        output.innerHTML += '<div class="right-text"><p><strong>' + "You" + '</strong> ' + moment(new Date().getTime()).format('h:mm a') + '</p>' + '<p>' + data.message + '</p></div><br>';
+    } else {
+        output.innerHTML += '<div><p><strong>' + data.username + '</strong> ' + moment(new Date().getTime()).format('h:mm a') + '</p>' + '<p>' + data.message + '</p></div><br>';
+    }
     feedback.innerHTML = '';
     document.querySelector('.chat-message').scrollTop = document.querySelector('.chat-message').scrollHeight
-
 })
 
 //Displaying if a user is typing
 socket.on('typing', (user) => {
     feedback.innerHTML = '<p><em>' + user + ' is typing...</em></p>';
 })
+socket.on("stopTyping", (user) => {
+    feedback.innerText = "";
+});
 
 //Displaying online users
-socket.on('online-users', (data) =>{
+socket.on('online-users', (data) => {
     users.innerHTML = ''
     data.forEach(user => {
         users.innerHTML += `<p>${user}</p>`
